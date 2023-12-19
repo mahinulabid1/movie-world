3b41a28ab3e6b39c23a845907be5e60351b461b9
Mahinul <himahinulabid@gmail.com>
2023-12-19 20:28:27 +0600

    modify routeHandler

--- movieworld/src/routeHandler.jsx
+++ movieworld/src/routeHandler.jsx
@@ -1,12 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import Homepage from "./app/homepage/homepage";
+import MovieDetailPage from "./app/movieDetailpage/movieDetail";
+
 
 const routeHandler = createBrowserRouter ( [
     {
         path : "/",
         element : <Homepage />,
     },
-    
+    {
+        path :  "/movieDetail",
+        element : <MovieDetailPage />
+    }
+
 ] );
 
 export default routeHandler;
