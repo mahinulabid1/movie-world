c883bbe2fc42794f332e192c8c2c8a1427f49dd2
Mahinul <himahinulabid@gmail.com>
2023-12-21 01:17:44 +0600

    connect component to database

--- movieworld/src/app/homepage/module/heroSection/heroSection.jsx
+++ movieworld/src/app/homepage/module/heroSection/heroSection.jsx
@@ -1,16 +1,29 @@
+import {useState, useEffect} from 'react'
 import styles from "./heroSection.module.css";
+import axios from "axios";
+
 
 const HeroSection = ( ) => {
+    const [movieThumbnail, setMovieThumbnail] = useState(undefined);
+    const [movieCover, setMovieCover] = useState(undefined);
+
+    useEffect(()=> {
+        axios.get('http://localhost:8000/featuredMovie?projection=movieCover-movieThumbnail&limit=1')
+        .then((result)=>{
+            const data = result.data[0];
+            setMovieThumbnail(`https://${data.movieThumbnail.url}`);
+            setMovieCover(`https://${data.movieCover.url}`)
+        })
+    }, []);
 
     return (
         <>
             <div 
-            className={"bound flex flex-align-center " + styles.container} 
-            style={{
-                background: "linear-gradient(to right, rgba(0,0,0,0.6), rgba(0,0,0,0.3)), url('./thumbnailBackground.jpg')",
-                backgroundSize: "cover",
-                /* Add other styles here */
-            }}
+                className={"bound flex flex-align-center " + styles.container} 
+                style={{
+                    background: `linear-gradient(to right, rgba(0,0,0,0.6), rgba(0,0,0,0.3)), url('${movieThumbnail}')`,
+                    backgroundSize: "cover"
+                }}
             >
 
                 <div className = { styles.textContainer }>
@@ -18,9 +31,17 @@ const HeroSection = ( ) => {
                     <h3> MOVIES AS YOU DEMAND AT USD</h3>
                     <h3 className={styles.priceInfo}> $10/MONTH </h3>
                 </div>
+                
                 <div>
                     <div className = { styles.sliderContainer }>
-                        <img className="border-radius"  src = "/thumbnailDemo.jpg" alt="" />
+
+                        {   // CONDITION BASED RENDERING
+                            movieCover !== "" ? 
+                            <img className="border-radius"  src = { movieCover } alt = { movieCover } loading='lazy'/>
+                            :
+                            console.log("fetching data")
+                        }
+                        
                         
                         <button className = { styles.leftButton } >
                             <img src="./leftIcon.svg" alt="" />
