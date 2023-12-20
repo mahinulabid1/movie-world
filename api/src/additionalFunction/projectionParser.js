a681394c053e79dae1a593f104d79c78fd7a70a1
Mahinul <himahinulabid@gmail.com>
2023-12-21 01:15:06 +0600

    create projection parser function

---
+++ api/src/additionalFunction/projectionParser.js
@@ -0,0 +1,10 @@
+const projectionParser = ( projection ) => {
+    /* 
+    - it takes a string INPUT similar to : "movieCover-movieThumbnail-movieName"
+    and then OUTPUTS : "movieCover movieThumbnail movieName"
+    */
+    let processedData = projection.replace("-", " ");
+    return(processedData);
+}
+
+module.exports = projectionParser;
