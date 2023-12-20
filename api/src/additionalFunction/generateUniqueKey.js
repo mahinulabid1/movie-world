6cc5ee99ee0123ff12fc927e2559a671f0f46e99
Mahinul <himahinulabid@gmail.com>
2023-12-20 18:26:02 +0600

    create unique key generator

---
+++ api/src/additionalFunction/generateUniqueKey.js
@@ -0,0 +1,10 @@
+// unique key generator
+function generateUniqueKey() {
+    const timestamp = Date.now().toString(36); // Convert current timestamp to base 36 string
+    const randomStr = Math.random().toString(36).substr(2, 10); // Random string
+    const hash = Math.random().toString(36).substr(2, 10); // Hash of random number
+    
+    return `${timestamp}-${randomStr}-${hash}`;
+}
+
+module.exports = {generateUniqueKey};
