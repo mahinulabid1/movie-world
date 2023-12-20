cb8a912e4b53aaf24746b9f99b841b4780038538
Mahinul <himahinulabid@gmail.com>
2023-12-21 01:15:55 +0600

    modify getData function

--- api/src/controller/movie/fetchData/getMovieData.js
+++ api/src/controller/movie/fetchData/getMovieData.js
@@ -1,14 +1,64 @@
 const MovieModel = require('../../../model/movieModel');
+const projectionParser = require('../../../additionalFunction/projectionParser');
+const { query } = require('express');
 
-// CONTAINS: functions that are related to database operations
-const getData = async (limit) => {
-    if(limit === undefined) {
-        const data = await MovieModel.find({});
-        return data;
-    } else if ( limit !== undefined && typeof limit === 'number'){
-        const data = await MovieModel.find({}).limit(limit);
-        return data;
+
+const getData = async ({queryLimit, condition, projection}) => {
+
+    projection === undefined ? projection = '' : projection = projection;
+    const parsedProjection = projectionParser( projection );
+
+    if( condition === undefined ) {
+        // fetch all data
+        if(queryLimit !== undefined) {
+            queryLimit = Number(queryLimit); 
+            const data = await MovieModel.find( { } ).limit(queryLimit);
+            return data;
+        }
+        else {
+            const data = await MovieModel.find( { } );
+            return data;
+        }
     }
+
+    else if( condition !== undefined ) {
+        // fetch data with filter
+        if( queryLimit !== undefined ) {
+            queryLimit = Number(queryLimit); 
+            // const parsedProjection = projectionParser( projection );
+            const data = await MovieModel.find(condition, parsedProjection).limit(queryLimit);
+            return data;
+        }
+        else {
+            const data = await MovieModel.find(condition, parsedProjection);
+            return data;
+        }
+    } 
+
+    // if( queryLimit === undefined && condition === undefined && projection === undefined ) {
+    //     // if everything is UNDEFINED
+    //     const data = await MovieModel.find( { } );
+    //     return data;
+    // } 
+    // else if ( queryLimit !== undefined && condition === undefined && projection === undefined ) {
+    //     // if only queryLimit is DEFINED
+    //     let limit = Number( queryLimit );
+    //     const data = await MovieModel.find( { } ).limit( limit );
+    //     return data;
+    // } 
+    // else if ( queryLimit === undefined && condition !== undefined && projection !== undefined ) {
+    //     //if "condition" and "projection" is DEFINED, "queryLimit" is UNDEFINED
+    //     const parsedProjection = projectionParser( projection );
+    //     const data = await MovieModel.find( condition,  parsedProjection);
+    //     return data;
+    // }
+    // else if ( queryLimit !== undefined && condition !== undefined && projection !== undefined ) {
+    //     //if "condition" and "projection" is DEFINED, "queryLimit" is UNDEFINED
+    //     const parsedProjection = projectionParser( projection );
+    //     let limit = Number( queryLimit );
+    //     const data = await MovieModel.find( condition,  parsedProjection).limit(limit);
+    //     return data;
+    // }
 }
 
 module.exports = getData;
