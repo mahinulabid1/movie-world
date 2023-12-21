36bcb9f786cb9741cef0f5637c1c7c1598600092
Mahinul <himahinulabid@gmail.com>
2023-12-21 14:21:13 +0600

    fix bound issue

--- movieworld/src/ui/navigation/navigation.jsx
+++ movieworld/src/ui/navigation/navigation.jsx
@@ -4,17 +4,20 @@ const Navigation = ( ) => {
 
     return (
         <>
-            <nav className={"flex bound flex-s-b flex-align-center " + styles.navContainer}>
-
+            <nav className={styles.navContainer}>
+                <div className="bound-margin flex  flex-s-b flex-align-center" style={{height: '100%'}}>
                 <a className={ styles.logo } href = "/" > <img src="./LOGO.png" alt=""  /> </a>
                 <input type="text" placeholder="Search Movies"/>
                 <a className={styles.getProButton} href="/getpro">Get Pro</a>
                 <a className={styles.links} href="/movies">Movies</a> 
                 <a className={styles.links} href="/watchlist">WatchList</a>
-                {/* <button>En DropIcon</button> */}
+
                 <button className={styles.userIcon}>
                     <img  src="./userIcon.svg" alt="" />
                 </button>
+                </div>
+
+                
 
 
 
