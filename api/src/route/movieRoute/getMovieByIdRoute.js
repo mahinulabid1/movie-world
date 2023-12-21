f9719be2ebf703875fe8d8d5de86ac93e344cc6e
Mahinul <himahinulabid@gmail.com>
2023-12-21 14:19:12 +0600

    finalize getMovieByIdRoute

--- api/src/route/movieRoute/getMovieByIdRoute.js
+++ api/src/route/movieRoute/getMovieByIdRoute.js
@@ -1,7 +1,14 @@
 // Goal : fetch specific movie details from database & send it as response
 // movie will be fetched using unique ID
 const {app}  = require("../../../index");
+const getMovieById = require('../../controller/movie/fetchData/getMovieById');
 
-app.get('/movieDetail', ( req, res ) => {
-    res.send('get movie detail using ID');
+app.get('/movieDetail',async ( req, res ) => {
+    let data = await getMovieById(req.query.id);
+    if(data !== ""){
+        res.send(data); 
+    }
+    else {
+        res.send('no data found');
+    }
 })
