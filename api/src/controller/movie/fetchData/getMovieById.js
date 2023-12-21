38a413fdb24c6a5a8cd3ab666f2e0b7de1eb1d4f
Mahinul <himahinulabid@gmail.com>
2023-12-21 14:20:49 +0600

    add/modify new function

---
+++ api/src/controller/movie/fetchData/getMovieById.js
@@ -0,0 +1,15 @@
+const MovieModel = require('../../../model/movieModel');
+
+const getMovieById =async (id) => {
+    if(id === undefined) {
+        return "id is not defined";
+    }
+    else{
+        console.log("running get movie by id")
+        const result = await MovieModel.findById(id);
+        return result;
+    }
+    
+}
+
+module.exports = getMovieById;
---
+++ api/src/route/movieRoute/getReleasedMovieRoute.js
@@ -0,0 +1,18 @@
+// Goal : fetch movies data with advance filtering
+const {app}  = require("../../../index");
+const getData = require('../../controller/movie/fetchData/getMovieData');
+
+app.get('/releasedMovie', async ( req, res ) => {
+    let result;
+    const queryLimit = req.query.limit;
+    const projection = req.query.projection; // what to fetch from upcoming movie document
+    let condition = {isReleased: true}
+    result = await getData({queryLimit, condition, projection});
+
+    if(result.length === 0) {
+        res.send("No data found!")
+    }else {
+        res.send(result);
+    }
+    
+})
