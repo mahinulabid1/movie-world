0b1f568c95288ed67bc7c7b8ea6831b147ee5ee9
Mahinul <himahinulabid@gmail.com>
2023-12-21 20:13:15 +0600

    make aws connection more secure

--- api/src/AWS_S3/initializeS3.js
+++ api/src/AWS_S3/initializeS3.js
@@ -1,10 +1,15 @@
 const AWS = require('aws-sdk');
 
+const s3AccessKeyId = process.env.AWS_S3_ACCESS_KEY_ID;
+const s3SecretAccessKey = process.env.AWS_S3_SECRET_ACCESS_KEY;
+const s3Region = process.env.AWS_S3_REGION;
+console.log(s3AccessKeyId);
+
 // Set up AWS configuration
 AWS.config.update({
-    accessKeyId: 'AKIAUYPP6YV4XQS665GF',
-    secretAccessKey: '9TND7wbQ8GCMeAFSOvPNrz0+k1gGdrZHJ2IHMizT',
-    region: 'ap-south-1' // Change to your AWS region
+    accessKeyId: s3AccessKeyId,
+    secretAccessKey: s3SecretAccessKey,
+    region: s3Region // Change to your AWS region
 });
 
 // initialize s3
